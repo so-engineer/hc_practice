104b3b3515dc35c55b498059670f666734f3841c
sousou1216 <sousou1216@yahoo.co.jp>
2023-12-05 07:53:04 +0900

    繰り返し処理を実装

--- python/randoms.py
+++ python/randoms.py
@@ -5,8 +5,11 @@ import random
 l = ["A", "B", "C", "D", "E", "F"]
 n = [2, 3]
 
-group_a = random.sample(l, random.choice(n))
-group_b = set(l) - set(group_a)
+for i in range(10):
+    group_a = random.sample(l, random.choice(n))
+    group_b = set(l) - set(group_a)
 
-print(sorted(group_a))
-print(sorted(list(group_b)))
+    print(f"{i+1}回目--------------------")
+    print(sorted(group_a))
+    print(sorted(list(group_b)))
+
